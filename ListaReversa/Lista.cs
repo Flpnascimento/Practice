e54c0381bb8d1c66f7b1ac21dd668a0a3508e3a1
Felipe Nascimento <flpnasciment@gmail.com>
2019-09-05 09:50:43 -0300

    Add project files.

---
+++ ListaReversa/Lista.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListaReversa
+{
+    class Lista
+    {
+        public void Listinha(int x)
+        {
+
+            var numbers = new double[x];
+
+            for (int i = 0; i < x;)
+            {
+                Console.WriteLine("Insira um numero no Array");
+
+                numbers.SetValue(Convert.ToDouble(Console.ReadLine()), i);
+
+                i++;
+            }
+
+            Console.WriteLine("O Array inicialmente te os seguintes valores:");
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("Precione Enter para ver o Array invertido");
+            Console.ReadLine();
+
+            Array.Reverse(numbers);
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ListaReversa/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ListaReversa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lista l = new Lista();
+
+                Console.WriteLine("Insira a quantidade de números do Array");
+                l.Listinha(Convert.ToInt32(Console.ReadLine()));
+
+
+            
+        }
+    }
+}
